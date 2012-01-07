ebd6bca136ba476d42ce0309748e04e54e0270b0
Matthew <pelargir@gmail.com>
2012-01-07 15:05:23 -0500

    tweaked template handler for Rails 3.1

--- lib/prawnto/template_handlers/base.rb
+++ lib/prawnto/template_handlers/base.rb
@@ -1,13 +1,24 @@
 module Prawnto
   module TemplateHandlers
-    class Base < (::Rails::VERSION::MAJOR < 3 ? ::ActionView::TemplateHandler : ::Object)
-      include ::ActionView::TemplateHandlers::Compilable if ::Rails::VERSION::MAJOR < 3
-      
-      def compile(template)
-        "_prawnto_compile_setup;" +
-        "pdf = Prawn::Document.new(@prawnto_options[:prawn]);" + 
-        "#{template.source}\n" +
-        "pdf.render;"
+    if ::Rails::VERSION::MAJOR < 3
+      class Base < ::ActionView::TemplateHandler
+        include ::ActionView::TemplateHandlers::Compilable
+
+        def compile(template)
+          "_prawnto_compile_setup;" +
+          "pdf = Prawn::Document.new(@prawnto_options[:prawn]);" + 
+          "#{template.source}\n" +
+          "pdf.render;"
+        end
+      end
+    else
+      class Base
+        def self.call(template)
+          "_prawnto_compile_setup;" +
+          "pdf = Prawn::Document.new(@prawnto_options[:prawn]);" + 
+          "#{template.source}\n" +
+          "pdf.render;"
+        end
       end
     end
   end
