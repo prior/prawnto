edc1928cf9671bdb8f5c1102048676684710f5ae
Forrest Zeisler <forrest@getjobber.com>
2012-01-22 14:40:59 -0800

    Fixing up ModelRenderer format ambiguity.

--- lib/prawnto/model_renderer.rb
+++ lib/prawnto/model_renderer.rb
@@ -1,17 +1,26 @@
 module Prawnto
   module ModelRenderer
+    
+    class CustomController < ApplicationController
+      def initialize
+        super
+        self.request = ActionDispatch::Request.new({})
+        self.response = ActionDispatch::Response.new()
+        self.params = {:format => :pdf}
+      end
+    end
+    
     # template : invoices/show.pdf
     # instance_variables : {"@account" => account} - variables set in before filters
     def self.to_string(template, calling_object = nil)
-      instance = ApplicationController.new
-      instance.request = ActionDispatch::Request.new({})
-      instance.response = ActionDispatch::Response.new()
-  
+      instance = CustomController.new
+
       if calling_object
         instance.prawnto :inline => true, :instance_variables_from => calling_object
       end
   
-      return instance.render_to_string(template, :template => false).html_safe
+      return instance.render_to_string(:action => template, :template => false, :format => :pdf).html_safe
     end
+
   end
 end
