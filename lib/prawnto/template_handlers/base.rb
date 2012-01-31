fb76d81277a4c44e41fe1888b0b4be2f948d93c8
Forrest Zeisler <forrest@getjobber.com>
2012-01-31 08:58:48 -0800

    Adding warning message for redefining 'pdf' in templates

--- lib/prawnto/template_handlers/base.rb
+++ lib/prawnto/template_handlers/base.rb
@@ -4,9 +4,19 @@ module Prawnto
   module TemplateHandlers
     class Base
       def self.call(template)
+        check_for_pdf_redefine(template.source)
+
         "_prawnto_compile_setup;" +
         "renderer = Prawnto::TemplateHandlers::Renderer.new(self);"+
         "renderer.to_pdf do; #{template.source}\nend;"
+      end
+
+    private
+
+      def self.check_for_pdf_redefine(template_source)
+        if template_source =~ /pdf\s*=[^=]/
+          Rails.logger.debug "\nPrawnto Warning! It looks like you may be redefining 'pdf' in your .prawn template. This no longer works in Prawnto 2+. Please use the :prawnto method in your controller\n"
+        end
       end
     end
   end
