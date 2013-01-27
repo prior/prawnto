40e5d3563b5fdd2d1dfd8d329f8eb54c23f7f7c5
Lewis Marshall <lewis.marshall@econsultancy.com>
2013-01-27 18:51:02 +0000

    Update template handlers for Rails 3.2

--- lib/prawnto/template_handlers/base.rb
+++ lib/prawnto/template_handlers/base.rb
@@ -1,9 +1,7 @@
 module Prawnto
   module TemplateHandlers
-    class Base < ::ActionView::TemplateHandler
-      include ::ActionView::TemplateHandlers::Compilable
-      
-      def compile(template)
+    class Base
+      def call(template)
         "_prawnto_compile_setup;" +
         "pdf = Prawn::Document.new(@prawnto_options[:prawn]);" + 
         "#{template.source}\n" +
