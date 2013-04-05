a6bb90cd572a0cd7962bc6251c09b6b64b5aad08
Chris Hooks <chris.hooks@econsultancy.com>
2013-04-05 16:12:27 +0100

    Update the call method to work with rails 3.2

--- lib/prawnto/template_handlers/base.rb
+++ lib/prawnto/template_handlers/base.rb
@@ -1,9 +1,13 @@
 module Prawnto
   module TemplateHandlers
     class Base
+      def self.call(template)
+        new.call(template)
+      end
+
       def call(template)
         "_prawnto_compile_setup;" +
-        "pdf = Prawn::Document.new(@prawnto_options[:prawn]);" + 
+        "pdf = Prawn::Document.new(@prawnto_options[:prawn]);" +
         "#{template.source}\n" +
         "raw pdf.render;"
       end
