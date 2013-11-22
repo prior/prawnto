8d5d62dddba84785d26641aaca350b5ecfb05237
William Billington <william.billington@econsultancy.com>
2013-11-22 15:48:01 +0000

    Don't redefine PDF mime type
    In rails 3.2 this is defined in rails/actionpack/lib/action_dispatch/http/mime_types.rb

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 require 'prawnto'
 
-Mime::Type.register "application/pdf", :pdf
+Mime::Type.register "application/pdf", :pdf unless Mime::PDF
 ActionView::Template.register_template_handler 'prawn', Prawnto::TemplateHandlers::Base
 ActionView::Template.register_template_handler 'prawn_dsl', Prawnto::TemplateHandlers::Dsl
 ActionView::Template.register_template_handler 'prawn_xxx', Prawnto::TemplateHandlers::Raw
